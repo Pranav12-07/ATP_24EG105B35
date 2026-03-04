a1e505fbae9f6231fd0c1c65e4e0a22a5b177cd1
Sai Pranav <sai@example.com>
2026-03-04 13:50:09 +0530

    Initial commit for weekly assignment

---
+++ week1/2_ass.js
@@ -0,0 +1,10 @@
+//2.Find the big number in given three numbers
+let  a=10
+let b=20
+let c=30
+if(a>b & a>c)
+    comsole.log('print a is greatest number',a)
+else if(b>c)
+    console.log('print b is greates number',b)
+else
+    console.log('print c is the greatest number',c)
---
+++ week2/1_a1.js
@@ -0,0 +1,26 @@
+const employees = [
+  { eno: 101, name: "Ravi", marks: [78, 82, 91] },
+  { eno: 102, name: "Bhanu", marks: [65, 70, 68] },
+  { eno: 103, name: "Sneha", marks: [88, 92, 95] },
+  { eno: 104, name: "Kiran", marks: [55, 60, 58] },
+  { eno: 105, name: "Anitha", marks: [90, 85, 87] },
+];
+//1
+employees.splice(1, 0, {
+  eno: 106,
+  name: "Pranav",
+  marks: [80, 85, 90]
+});
+//2
+let index = employees.findIndex(emp => emp.name === "Kiran");
+
+if (index !== -1) {
+  employees.splice(index, 1);
+}
+
+//3
+let sneha = employees.find(emp => emp.name === "Sneha");
+
+if (sneha) {
+  sneha.marks[sneha.marks.length - 1] = 75;
+}
