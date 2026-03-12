ec60265ce22170b5e93827cd7e8adbea9bbfb9e0
Sai Pranav <saipranav1207@gmail.com>
2026-03-12 19:15:37 +0530

    Delete week2_aasignment directory

--- week2_aasignment/ass12.js
+++
@@ -1,8 +0,0 @@
-// Create a function that receives any number of args 
-// as arguments and return their sum using REST parameter
-
-let findSum = (...numbers) => {
-    return numbers.reduce((sum, element) => sum + element)
-}
-
-console.log(findSum(5, 15, 25, 35, 45))
