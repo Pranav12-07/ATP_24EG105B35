8ae3ff65144e4bbf41ad0a9f253f57f1e331f019
Sai Pranav <saipranav1207@gmail.com>
2026-03-25 09:58:46 +0530

    Delete week4/week1 directory

--- week4/week1/1_ass.js
+++
@@ -1,8 +0,0 @@
-//1.Find the big number in given two numbers
-let a=10
-let b=20
-if(a>b)
-    console.log('a is greates number',a)
-else
-    console.log('print b is greates',b)
-    
--- week4/week1/4_ass.js
+++
@@ -1,7 +0,0 @@
-//4. Find the smallest element in marks array
-let a=[10,20,9,12]
-let min=a[0]
-for(let i = 0; i < a.length ; i++)
-    if(a[i]<min) 
-        min=a[i]
-    console.log(min)
